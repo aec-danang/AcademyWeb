ea503fbfccff47faa0289943eb490ad7e2436d94
Joshi Minh <binhangia241273@gmail.com>
2026-09-16 20:52:28 +0700

    fix(config): refine Cloudinary remotePatterns pathname and ensure clean syntax

--- next.config.ts
+++ next.config.ts
@@ -13,7 +13,7 @@ const nextConfig: NextConfig = {
       {
         protocol: "https",
         hostname: "res.cloudinary.com",
-        pathname: "/:cloudName/**",
+        pathname: "/*/**",
       },
     ],
   },
